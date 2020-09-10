fe8dbf6940ae0b0fbbce72ba48200bbdc497d6e7
Bill Mårtensson <bill@magictechnology.se>
2020-09-10 09:20:25 +0200

    Måndagens kod

--- pia9intro/ViewController.swift
+++ pia9intro/ViewController.swift
@@ -9,11 +9,24 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    @IBOutlet weak var thetextthing: UILabel!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        
+        thetextthing.text = "Start av app"
+        
     }
 
+    @IBAction func clickingbutton(_ sender: Any) {
+        
+        thetextthing.text = "Klickat på knapp"
+    }
+    
 
 }
 
